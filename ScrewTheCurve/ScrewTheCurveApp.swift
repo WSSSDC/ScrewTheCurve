9c87d3bb6353820e740b97846957087d8f2c2448
Bram <ogus.bram@gmail.com>
2022-01-21 19:52:16 -0500

    Initial Commit

---
+++ ScrewTheCurve/ScrewTheCurveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrewTheCurveApp.swift
+//  ScrewTheCurve
+//
+//  Created by Bram Ogus on 2022-01-21.
+//
+
+import SwiftUI
+
+@main
+struct ScrewTheCurveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
