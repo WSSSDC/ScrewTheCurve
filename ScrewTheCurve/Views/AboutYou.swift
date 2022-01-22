ed2af0f30c99f30ceb6d05cdbe484468938bc191
Arjun Sarao <arjunsarao727@gmail.com>
2022-01-22 16:54:20 -0500

    working on the bio

--- ScrewTheCurve/Views/AboutYou.swift
+++ ScrewTheCurve/Views/AboutYou.swift
@@ -6,10 +6,33 @@
 //
 
 import SwiftUI
-
 struct AboutYou: View {
+    @State private var biography: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("bg_purple")
+                .ignoresSafeArea()
+            VStack {
+                Text("About Me")
+                    .font(.system(size: 35))
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+
+                Spacer()
+                Image("profile1")
+                    .resizable()
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .frame(width: 150.0, height: 150.0)
+                    .offset(y:-550)
+                Text("Bio")
+                    .offset(x:-170, y:-550)
+                    .font(.system(size: 25))
+                TextField("Please enter a short about your here:", text: $biography)
+                    .offset(x:20, y:-550)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+        }
+
     }
 }
 
@@ -17,4 +40,5 @@ struct AboutYou_Previews: PreviewProvider {
     static var previews: some View {
         AboutYou()
     }
+}
 }
