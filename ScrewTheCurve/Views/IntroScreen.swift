e164b806bca7e22d9bf1c1a1d0b687da399f7bb4
Bram <ogus.bram@gmail.com>
2022-01-21 22:31:26 -0500

    Moved intro screen UI to proper file

--- ScrewTheCurve/Views/IntroScreen.swift
+++ ScrewTheCurve/Views/IntroScreen.swift
@@ -9,33 +9,31 @@ import SwiftUI
 
 struct IntroScreen: View {
     var body: some View {
-        NavigationView{
             ZStack{
-                Ellipse()
-                    .fill(Color("bg_purple"))
-                    .frame(width: 1250, height: 1000)
-                    .offset(x: 0, y: 550)
-                VStack {
-                    Text("Make a friend,")
-                        .font(.largeTitle)
-                    Text("Expand your connections!")
-                        .font(.largeTitle)
-                    Text("Having lots of friends will")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                    Text("make your life more colorful!")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                }.offset(x: 0, y: 150)
-                NavigationLink(destination: SignUp()){
-                    Circle()
-                        .fill(Color("nav_black"))
-                        .frame(width: 60, height: 60)
-                        .shadow(color: .black, radius: 2)
-                        .offset(y:325)
-                }
-
+            Ellipse()
+                .fill(Color("bg_purple"))
+                .frame(width: 1250, height: 1000)
+                .offset(x: 0, y: 550)
+            VStack {
+                Text("Make a friend,")
+                    .font(.largeTitle)
+                Text("Expand your connections!")
+                    .font(.largeTitle)
+                Text("Having lots of friends will")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                Text("make your life more colorful!")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }.offset(x: 0, y: 150)
+            NavigationLink(destination: SignUp()){
+                Circle()
+                    .fill(Color("nav_black"))
+                    .frame(width: 60, height: 60)
+                    .shadow(color: .black, radius: 2)
+                    .offset(y:325)
             }
+
         }
     }
 }
