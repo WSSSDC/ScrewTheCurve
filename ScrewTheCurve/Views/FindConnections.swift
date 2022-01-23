e0a0d8348a99f20dedc55bc957668d6a623a3409
Arjun Sarao <arjunsarao727@gmail.com>
2022-01-22 22:04:50 -0500

    working on the connectionfinder

--- ScrewTheCurve/Views/FindConnections.swift
+++ ScrewTheCurve/Views/FindConnections.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct FindConnections: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            HStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color("bg_purple"))
+                        .frame(width: 400, height: 700)
+                    Image("profile1")
+                        .resizable()
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .frame(width: 400, height: 400)
+                        .offset(y:-150)
+                    Text("Arjun Sarao, 18")
+                        .font(.system(size: 35))
+                        .offset(x:-80, y:80)
+                    Text("Hello, I am a 2nd year Data Science student, I am looking for friends to learn and work on projects with!")
+                        .font(.system(size: 25))
+                        .padding()
+                        .offset(y:150)
+                }
+            }
+        }
     }
 }
 
